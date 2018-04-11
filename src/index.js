e4d4fac487a59f524029131e0d7a8e3a4bf7d7ef
Don Carron <Don.Carron@corp.int>
2018-04-11 11:40:33 -0700

    Added support for regular dependencies. Added dist back in because I'm lazy and don't want to publish to NPM yet.

--- src/index.js
+++ src/index.js
@@ -2,16 +2,24 @@ import { either, pipe } from 'ramda';
 import externalToFn from './external-to-fn';
 import getModulesMatcher from './get-modules-matcher';
 import getPeerDeps from './get-peer-deps';
+import getDeps from './get-deps';
 
-export default function PeerDepsExternalPlugin({packageJsonPath} = {}) {
+export default function PeerDepsExternalPlugin({packageJsonPath, doIncludeDeps} = {}) {
+  let deps = getPeerDeps(packageJsonPath);
+
+  if(doIncludeDeps) {
+    deps = deps.concat(getDeps(packageJsonPath))
+  }
+
   return {
     name: 'peer-deps-external',
     options: opts => {
       opts.external = either(
         // Retain existing `external` config
         externalToFn(opts.external),
+
         // Add `peerDependencies` to `external` config
-        getModulesMatcher(getPeerDeps(packageJsonPath))
+        getModulesMatcher(deps)
       );
 
       return opts;
